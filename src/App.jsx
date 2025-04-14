c819c7352cbebf467427986ede8827df030f9274
조아 <parkjoah@naver.com>
2025-04-14 19:11:55 +0900

    feat: font, img, layout

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Home from "./pages/Home";
 import New from "./pages/New";
 import Diary from "./pages/Diary";
 import Notfound from "./pages/Notfound";
-
+import { getEmotionImage } from "./util/get-emotion-image";
 //1. "/" : 모든 일를 조회하는 home 페이지
 //2. "/new" : 새로운 일기를 작성하는  new 페이지
 //3. "/diary" : 일기를 상세히 조회하는 diary 페이지
@@ -17,6 +17,13 @@ function App() {
   return (
     <>
       <div>
+        <img src={getEmotionImage(1)} />
+        <img src={getEmotionImage(2)} />
+        <img src={getEmotionImage(3)} />
+        <img src={getEmotionImage(4)} />
+        <img src={getEmotionImage(5)} />
+      </div>
+      <div>
         <Link to={"/"}>Home</Link>
         <Link to={"/new"}>New</Link>
         <Link to={"/diary"}>Diary</Link>
@@ -25,7 +32,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/new" element={<New />} />
-        <Route path="/diary" element={<Diary />} />
+        <Route path="/diary/:id" element={<Diary />} />
         <Route path="*" element={<Notfound />} />
       </Routes>
     </>
