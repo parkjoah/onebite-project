b6df1f88dd906ce8c669e55be8503a9f62a9de56
조아 <parkjoah@naver.com>
2025-04-15 21:56:43 +0900

    feat: 공통 컴포넌트

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,44 @@
 import "./App.css";
-import { Route, Routes, Link, useNavigate } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import New from "./pages/New";
 import Diary from "./pages/Diary";
 import Notfound from "./pages/Notfound";
-import { getEmotionImage } from "./util/get-emotion-image";
+import Button from "./components/Button";
+import Header from "./components/Header";
+
 //1. "/" : 모든 일를 조회하는 home 페이지
 //2. "/new" : 새로운 일기를 작성하는  new 페이지
 //3. "/diary" : 일기를 상세히 조회하는 diary 페이지
 function App() {
-  const nav = useNavigate();
-
-  const onClickButton = () => {
-    nav("/new");
-  };
   return (
     <>
-      <div>
-        <img src={getEmotionImage(1)} />
-        <img src={getEmotionImage(2)} />
-        <img src={getEmotionImage(3)} />
-        <img src={getEmotionImage(4)} />
-        <img src={getEmotionImage(5)} />
-      </div>
-      <div>
-        <Link to={"/"}>Home</Link>
-        <Link to={"/new"}>New</Link>
-        <Link to={"/diary"}>Diary</Link>
-      </div>
-      <button onClick={onClickButton}>New 페이지로 이동</button>
+      <Header
+        title={"Header"}
+        leftChild={<Button text={"Left"} />}
+        rightChild={<Button text={"right"} />}
+      />
+      <Button
+        text={"123"}
+        onClick={() => {
+          console.log("123번 버튼 클릭");
+        }}
+      />
+      <Button
+        text={"123"}
+        type={"POSITIVE"}
+        onClick={() => {
+          console.log("123번 버튼 클릭");
+        }}
+      />
+      <Button
+        text={"123"}
+        type={"NEGATIVE"}
+        onClick={() => {
+          console.log("123번 버튼 클릭");
+        }}
+      />
+
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/new" element={<New />} />
