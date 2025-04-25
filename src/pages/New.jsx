9140bc8014d3350ff0b45ab2da70858d28c7a2dc
조아 <parkjoah@naver.com>
2025-04-25 20:28:22 +0900

    feat: new페이지 기능

--- src/pages/New.jsx
+++ src/pages/New.jsx
@@ -1,15 +1,24 @@
 import Button from "../components/Button";
 import Editor from "../components/Editor";
 import Header from "../components/Header";
+import { useNavigate } from "react-router-dom";
+import { useContext } from "react";
+import { DiaryDispatchContext } from "../App";
 
 const New = () => {
+  const nav = useNavigate();
+  const { onCreate } = useContext(DiaryDispatchContext);
+  const onSubmit = (input) => {
+    onCreate(input.createdDate.getTime(), input.emotionId, input.content);
+    nav("/", { replace: true });
+  };
   return (
     <div>
       <Header
         title={"새 일기 쓰기"}
-        leftChild={<Button text={"< 뒤로 가기"} />}
+        leftChild={<Button text={"< 뒤로 가기"} onClick={() => nav(-1)} />} //nav인수로 -1 넣어주면 페이지를 뒤로이동
       />
-      <Editor />
+      <Editor onSubmit={onSubmit} />
     </div>
   );
 };
